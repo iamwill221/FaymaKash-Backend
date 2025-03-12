6bd69a10bcbbe0d50f85a7f9f61d55240ca7377d
iamwill221 <ousmanewilliamk@gmail.com>
2025-03-12 13:36:15 +0000

    Correction of a bug with twilio : Refactor OTP functions to remove spaces from phone numbers before processing

--- PaymentSystem/utils.py
+++ PaymentSystem/utils.py
@@ -8,7 +8,7 @@ from FaymaKashProject import settings
 # Function to send OTP via Twilio Verify
 def send_otp_via_twilio(phone_number):
     # Convert PhoneNumber object to string if it's not already
-    phone_str = str(phone_number)
+    phone_str = str(phone_number).replace(' ', '')  # Remove any spaces
     
     client = Client(settings.TWILIO_ACCOUNT_SID, settings.TWILIO_AUTH_TOKEN)
     verification = client.verify \
@@ -21,11 +21,14 @@ def send_otp_via_twilio(phone_number):
 
 # Function to check the OTP entered by the user
 def check_otp_via_twilio(phone_number, otp):
+    # Convert PhoneNumber object to string if it's not already
+    phone_str = str(phone_number).replace(' ', '')  # Remove any spaces
+    
     client = Client(settings.TWILIO_ACCOUNT_SID, settings.TWILIO_AUTH_TOKEN)
 
     # Check the verification status
     verification_check = client.verify.v2.services(settings.TWILIO_VERIFY_SERVICE_SID) \
-        .verification_checks.create(to=phone_number, code=otp)
+        .verification_checks.create(to=phone_str, code=str(otp))
 
     return verification_check.status == 'approved'  # Return True if the OTP is valid
 
